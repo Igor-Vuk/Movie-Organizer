44536cc30d5a2af1822a104b4901c1e15e456ed1
Igor Vukelić <igvukelic@gmail.com>
2017-02-13 08:10:04 +0100

    modify for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 const PATH = require("path");
-const PORT = process.env.IP || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
